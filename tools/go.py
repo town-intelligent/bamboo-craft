0b64e583009e33a60c92f0bc422601888037cbfc
yillkid <yillkid@gmail.com>
2021-11-19 16:05:18 +0800

    feat: add indea pgae link

--- tools/go.py
+++ tools/go.py
@@ -1,44 +1,29 @@
-def replace(item, list_content):
-    f = open("./source.html")
-    list_source = f.readlines()
-    f.close()
+import os
 
-    counters = 0
+def replace(filename):
+    file_target = open(filename, "r")
+    list_source = file_target.readlines()
+    file_target.close()
+
     for index in range(len(list_source)):
-        if "CONTENT" == list_source[index].rstrip("\n"):
+
+        if "h2 font-weight-bold" in list_source[index].rstrip("\n"):
             del list_source[index]
-
-
-            counter  = index
-            items = 0
-            for obj in list_content:
-                items = items + 1
-                list_source.insert(counter, "<p>" + obj.rstrip("\n"))
-                counter = counter + 1
+            list_source.insert(index, "<p class=\"h2 font-weight-bold\"><a href=\"../index.html\">竹山竹藝</a></p>")
 
             # File
-            textfile = open("output/" + str(item-1) + ".html", "w")
+            textfile = open("output/" + filename, "w")
             for element in list_source:
                 textfile.write(element.rstrip("\n") + "\n")
+        
             textfile.close()
 
             return
 
-# Read input
-f = open("./input.txt")
-lines_input = f.readlines()
-f.close()
 
-items = 1
-for index in range(len(lines_input)):
-    if lines_input[index].rstrip("\n") == str(items):
-        content = []
-        items = items + 1
+# List dir
+list_file = os.listdir("./")
 
-        counter = index
-        while lines_input[counter].rstrip("\n") != str(items):
-            content.append(lines_input[counter].rstrip("\n"))
-            counter = counter + 1
-
-        # Replace
-        replace(items-1, content)
+for str_filename in list_file:
+  if ".html" in str_filename:
+      replace(str_filename)
---
+++ tools/go.sh
@@ -0,0 +1,5 @@
+rm *.html
+rm output/*.html
+cp ../*.html .
+python3 go.py
+cp output/*.html ../
